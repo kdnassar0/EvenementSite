b6f97705d911a2ae44b9575fc2d1c9b8fbb387bc
kenan nassar <kdnassar0@gmail.com>
2023-03-01 11:15:06 +0100

    supprimer un evenement par la base des donnees

--- src/Controller/EvenamentController.php
+++ src/Controller/EvenamentController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
 use App\Entity\User;
 use App\Repository\EvenementRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,7 +19,6 @@ class EvenamentController extends AbstractController
     {
 
       $evenementsPassees = $e-> findEvenementsPassees() ;
-     
       $evenementsEncours = $e-> findEvenementsEncours() ;
 
    
@@ -29,8 +30,25 @@ class EvenamentController extends AbstractController
           'evenementsEncours' => $evenementsEncours, 
           'evenementsPassees'=>$evenementsPassees, 
            
+
         ]);
     }
+
+    /**
+     *@Route("/evenement/{id}/suprimmer",name="supprimer_evenement")
+     */
+
+     public function supprimerEvenement(Evenement $evenement , ManagerRegistry $doctrine)
+     {
+
+        $entityManager =$doctrine->getManager() ; 
+        $entityManager->remove($evenement) ; 
+        $entityManager->flush() ;
+
+        return $this->redirectToRoute('app_evenament') ;
+
+
+     }
 
 
 }
--- src/Repository/EvenementRepository.php
+++ src/Repository/EvenementRepository.php
@@ -84,6 +84,8 @@ class EvenementRepository extends ServiceEntityRepository
        
     }
 
+    
+
 
     
 
