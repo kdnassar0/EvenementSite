c9c90b7a8551fb95a666951ebf58e60b0a560b7a
kenan nassar <kdnassar0@gmail.com>
2023-06-13 09:57:14 +0200

    image Affiche par defult

--- src/Entity/Evenement.php
+++ src/Entity/Evenement.php
@@ -96,7 +96,7 @@ class Evenement
     private $lieu;
 
     /**
-     * @ORM\Column(type="string", length=1000, nullable=true,,options={"default" : "'chemin/vers/image_par_defaut.jpg'"})
+     * @ORM\Column(type="string", length=1000, nullable=true, options={"default": "affiche-de-l-evenement-1.png"})
      */
     private $imageAffiche;
 
